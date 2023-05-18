64d39b4db020a147e31e2a1ac415eeb174da2a3a
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-05-18 18:09:32 +0200

    Composer.json not found fix

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+composer install --no-progress --no-interaction
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo ".env file already exists."
+fi
+
+php artisan migrate
+php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+
+php artisan serve --port=$PORT --host=0.0.0.0
+
+php artisan migrate
+exec docker-php-entrypoint "$@"
