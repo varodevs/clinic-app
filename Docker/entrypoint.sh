f1179a9c00dc3b3618393afcd38a427365f7066a
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-05-18 18:43:23 +0200

    Composer.json not found fix

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "vendor/autoload.php" ]; then
 composer install --no-progress --no-interaction
-fi
 
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
