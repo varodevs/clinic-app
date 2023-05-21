697d5728bf4da5d673b887c11286465e383638d0
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-05-21 13:06:55 +0200

    Nginx server add

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-composer install --no-progress --no-interaction
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
 
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
