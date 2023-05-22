eeb7d1f76a92c826771b9ccbae592a8c38acb018
EC2 Default User <ec2-user@ip-172-31-24-27.ec2.internal>
2023-05-22 14:59:49 +0000

    Production version

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+                host: 'ec2-18-213-223-134.compute-1.amazonaws.com',
+            },
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss',
-            'resources/css/app.css',
-            'resources/js/app.js'],
+            input: ['/var/www/html/resources/sass/app.scss',
+                '/var/www/html/resources/css/app.css',
+                '/var/www/html/resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
