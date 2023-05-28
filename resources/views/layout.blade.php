b07364caaa266a991dc8b9724f8d08e46c7cf35c
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-05-28 13:14:42 +0200

    commit logo img css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="{{ asset("/img/favicon-16x16.png") }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>ActiveLife Physio</title>
@@ -45,7 +46,7 @@
             </div>
           </nav>
           <div class="align-self-center w-50 m-1 _menu">
-            <h1><img class="_logo-img" src="{{ asset('img/logo.png') }}" alt="Logo img"> ActiveLife</h1>
+            <div id="logo" class="d-flex"><img src="{{ asset('img/logo.png') }}" alt="Logo img" height="50vh"><h1>ActiveLife</h1></div>
             <ul class="nav">
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
