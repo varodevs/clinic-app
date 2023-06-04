8102c606d36e1dfde7985c173df3c88cf31f6f83
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 01:13:07 +0200

    appointments

--- resources/views/profile-emp.blade.php
+++ resources/views/profile-emp.blade.php
@@ -15,11 +15,11 @@
         </div>
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Birth date</span>
-            <input type="text" class="form-control" placeholder="Last name" aria-label="ID" aria-describedby="basic-addon1" value="{{ $carbonDate = Carbon::createFromFormat('Y/m/d', $employee->date_birth); }}" disabled>
+            <input type="text" class="form-control" placeholder="Last name" aria-label="ID" aria-describedby="basic-addon1" value="{{ $carbonDate = \Carbon\Carbon::createFromFormat('Y/m/d', $employee->date_birth); }}" disabled>
         </div>
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Date hired</span>
-            <input type="text" class="form-control" placeholder="Last name" aria-label="ID" aria-describedby="basic-addon1" value="{{ $carbonDate = Carbon::createFromFormat('Y/m/d', $patient->date_hire); }}" disabled>
+            <input type="text" class="form-control" placeholder="Last name" aria-label="ID" aria-describedby="basic-addon1" value="{{ $carbonDate = \Carbon\Carbon::createFromFormat('Y/m/d', $patient->date_hire); }}" disabled>
         </div>
         <div class="input-group mb-3">
             <label for="image">Upload Image:</label>
