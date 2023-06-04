8acbf7f32a4797716c597e242495dde75e79ae38
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-04 17:34:32 +0200

    appointments

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -34,7 +34,7 @@
                                 <label class="_form-label" for="email">Email</label>
                                 <input class="form-control" type="email" name="email" id="email" placeholder="name@email.com">
                                 <label class="_form-label" for="date">Check for dates</label>
-                                <input class="form-control" type="datetime-local" name="date" id="date">
+                                <input class="form-control" type="datetime-local" name="date" id="datetime">
                                 <label class="_form-label" for="id">ID</label>
                                 <input class="form-control" type="text" name="id" id="id">
                                 <label class="_form-label">Underage?</label>
@@ -62,25 +62,40 @@
     </div>
     <script>
         $(document).ready(function() {
-          var disabledDates = ["2023-06-05", "2023-06-12", "2023-06-19"];
-        
-          $('#date').on('change', function() {
-            var selectedDate = $(this).val();
-        
-            if (disabledDates.includes(selectedDate)) {
-              $(this).val('');              
+            // Array of disabled dates
+            var disabledDates = ['2023-05-28', '2023-06-05', '2023-06-12'];
+            
+            // Function to check if a date is disabled
+            function isDateDisabled(date) {
+                var formattedDate = date.toISOString().split('T')[0];
+                return disabledDates.includes(formattedDate);
             }
-          });
-        
-          $('#date').on('focus', function() {
-            $(this).attr('min', '2023-06-01');
-            $(this).attr('max', '2023-06-30');
-        
-            disabledDates.forEach(function(date) {
-              $(this).removeAttr('disabled');
-              $('input[type="date"][value="' + date + '"]').attr('disabled', 'disabled');
-            });
-          });
+            
+            // Function to set the min and max attributes of the input field
+            function setDateTimeLimits() {
+                var minDate = new Date();
+                var maxDate = new Date();
+                maxDate.setFullYear(maxDate.getFullYear() + 1);
+                
+                $('#datetime').attr('min', minDate.toISOString().slice(0, 16));
+                $('#datetime').attr('max', maxDate.toISOString().slice(0, 16));
+            }
+            
+            // Function to handle change event of the input field
+            function handleDateTimeChange() {
+                var selectedDate = new Date($('#datetime').val());
+                
+                if (isDateDisabled(selectedDate)) {
+                    $('#datetime').val('');
+                    alert('This date is disabled. Please select another date.');
+                }
+            }
+            
+            // Set initial date limits
+            setDateTimeLimits();
+            
+            // Attach change event handler
+            $('#datetime').on('change', handleDateTimeChange);
         });
         </script>
 @endsection
