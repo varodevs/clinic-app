6179bb57914f50669b813c5636095b0f4cc7745a
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-04 16:10:51 +0200

    appointments

--- resources/views/profile-info.blade.php
+++ resources/views/profile-info.blade.php
@@ -1,5 +1,5 @@
 <div class="col-2 align-self-start m-3">
-    <form action="{{ route('upload-image') }}" method="POST" enctype="multipart/form-data">
+    <form action="#" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">First Name</span>
