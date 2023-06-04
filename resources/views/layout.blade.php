02f4c02a52ede8806494dc19a74356ad8f91b9e2
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 01:21:00 +0200

    appointments

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -79,7 +79,7 @@
                 <li class="nav-item">
                   <a class="nav-link" href="{{ route('about') }}">About us</a>
                 </li>
-                @if (session('role') != 6)
+                @if (session('role') == 4 || (session('role') == 5)
                 <li class="nav-item">
                   <a class="nav-link" href="{{  route('admin')  }}">Admin</a>
                 </li>
