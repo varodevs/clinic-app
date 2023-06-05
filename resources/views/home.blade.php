74d84e7b6598537738091cda52c982ff08864291
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 08:00:42 +0200

    maps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -104,10 +104,9 @@
                     </form>
                 </div>
             </div>
-            <div>
+            <div class="w-25">
                 @include('Map');
             </div>
-            <div class="w-25"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
         </div>
     </div>
 </div>
