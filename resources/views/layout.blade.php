ade931e8cfb557b076f4048fce47d0d1e6519cba
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 12:54:47 +0200

    maps

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key={{ config('app.google_maps_api_hey') }}&callback=initMap" async defer></script>
     <title>ActiveLife Physio</title>
 </head>
 <body class="d-flex flex-column min-vh-100">
