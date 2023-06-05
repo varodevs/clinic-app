ad3ed37573d2e6f670bc61f74a2c3efe598ea590
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 04:10:15 +0200

    stripe

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
-    //
+    public function stripe_view(){
+
+        return view(view:'stripe');
+
+    }
+
+    public function checkout(){
+
+        \Stripe\Stripe::setApiKey(config(key:'stripe.sk'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'gbp',
+                        'product_data' => [
+                            'name' => 'Session Physio',
+                        ],
+                        'unit_ammount' => 5,
+                    ],
+                    'quantity' => 1,
+                ],
+            ],
+            'mode'        => 'payment',
+            'success_url' => route(name:'success'),
+            'cancel_url'  => route(name:'stripe'),
+        ]);
+
+        return redirect()->away($session->url);
+    }
+
+    public function success(){
+        return view(view:'stripe');
+    }
 }
