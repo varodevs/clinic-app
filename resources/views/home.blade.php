d11b55605742392f0ac2ee21dae0b2ef72b3b27c
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 05:33:33 +0200

    stripe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
     </div>
     
     <div class="c-12 d-flex flex-column align-items-center pt-5 _div-treat" mb-5>
-        <div class="text-center w-50 m-3"><h4>TREATMENTS</h4></div>
+        <div class="c-3 text-center w-50 m-3"><h4>TREATMENTS</h4></div>
         <div id="carouselExampleControls" class="carousel slide w-25" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active w25">
+                <div class="carousel-item active">
                     <div class="c-3 d-flex flex-column align-self-center text-center">
                         <h5>MASOTERAPIA</h5>
                         <img src="{{ asset('img/maso.jpg') }}" class="_img-carou" alt="..." width="100%" height="200vh">                    
