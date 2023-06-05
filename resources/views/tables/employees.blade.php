352a1ba0a7c000debe61a3b2e35793e2bb6fb176
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 03:18:43 +0200

    appointments

--- resources/views/tables/employees.blade.php
+++ resources/views/tables/employees.blade.php
@@ -22,9 +22,9 @@
                 <tr>
                     @foreach ($row as $column)
                     @if ($i == 4 && $i == 5)
-                    <td><input class="w-50" type="date" value="{{ \Carbon\Carbon::parse($column)->format('Y-m-d H:i:s') }}" name="date{{ $i }}"/></td>
+                    <td class="w-50"><input class="w-50 text-center" type="date" value="{{ \Carbon\Carbon::parse($column)->format('Y-m-d H:i:s') }}" name="date{{ $i }}"/></td>
                     @else
-                    <td><input class="w-50" type="text" value="{{ $column }}" name="input{{ $i }}"/></td>
+                    <td class="w-50"><input class="w-50 text-center" type="text" value="{{ $column }}" name="input{{ $i }}"/></td>
                     @endif
                     @php                                
                     if ($i == 0) {
