6d80bab4f21a006841994a4a79f3d1af61dd298d
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 06:57:50 +0200

    stripe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,8 +67,8 @@
     </div>
     <div class="c-12 d-flex justify-content-evenly pt-5" mb-5>
         <div class="c-12 text-center w-50 m-3"><h4 class="align-self-center">ABOUT US</h4></div>
-        <div>
-            <div class="c-12 d-flex justify-content-evenly pt-5">
+        <div  class="c-12 d-flex justify-content-evenly pt-5">
+            <div>
                 <img src= {{ asset('img/equipo.jpg') }} height="250vh" />
             </div>
             <div class="w-25"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
