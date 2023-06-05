2c81dbc7c699a2b0c2d95958069e91aeffcb9192
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 13:31:07 +0200

    maps

--- resources/js/mapInput.js
+++
@@ -1,8 +0,0 @@
-const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: latitude, lng: longitude},
-    zoom: 13
-});
-const marker = new google.maps.Marker({
-map: map,
-position: {lat: latitude, lng: longitude},
-});
