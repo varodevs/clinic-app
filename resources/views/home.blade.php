ee03a19d60e70233ac3c67df2342db9fe7bd8e56
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-05 08:18:19 +0200

    maps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,35 +75,9 @@
         </div>
     </div>
     <div>
-
-                <div class="c-4 d-flex flex-column justify-content-center _div-login">
-                    <form class="d-flex flex-column justify-content-center _form" action="" method="POST">
-                    @csrf
-                    <div class="d-flex justify-content-evenly w-75">
-                        <div>
-                            <label class="_form-label" for="fname">First Name</label>
-                            <input class="form-control" type="text" name="fname" id="fname">
-                            <label class="_form-label" for="sname">Phone Number</label>
-                            <input class="form-control" type="text" name="sname" id="sname">
-                        </div>
-                        <div>
-                            <label class="_form-label" for="lname">Last Name</label>
-                            <input class="form-control" type="text" name="lname" id="lname">
-                            <label class="_form-label" for="email">Email</label>
-                            <input class="form-control" type="email" name="email" id="email" placeholder="name@email.com">
-                            
-                        </div>
-                    </div>
-                        <div class="w-75">
-                            <label class="_form-label" for="textarea" >Describe your problem:</label>
-                            <textarea class="form-control" name="textarea" id="textarea"></textarea>
-                        </div>
-                    
-                    </form>
-                </div>
-                <div class="w-75">
-                    @include('Map')
-                </div>
+        <div class="w-75">
+            @include('Map')
+        </div>
     </div>
 </div>
 @endsection
