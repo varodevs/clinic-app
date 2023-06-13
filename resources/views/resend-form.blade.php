565b6ebfa66f515855a94708f39fb594d9cbf39d
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-13 22:05:15 +0200

    new user

--- resources/views/resend-form.blade.php
+++ resources/views/resend-form.blade.php
@@ -6,10 +6,13 @@
         <div class="container-fluid d-flex flex-column justify-content-center _div-azul" id="section">
             <h3 class="mx-3">Email Verification</h3>
         </div>
+        <div class="container-fluid d-flex flex-column align-items-center pt-5">
+            <div class="text-center w-50"><h2>Verify that your email exists</h2></div>
+            <div class="text-center w-75"><p>If your email belongs to an user you will receive an email to reset your password.</p></div>
+        </div>
         <div class="d-flex justify-content-center _content">
             <div class="d-flex flex-column justify-content-center">
-                <h4>Verify that your email exists</h4>
-                <p>If your email belongs to an user you will receive an email to reset your password.</p>
+
                 <div class="d-flex flex-column justify-content-center px-5 py-3 _div-login">
                     <form class="d-flex flex-column justify-content-center _form" action="{{ route('reset-email') }}" method="POST">
                         @csrf
