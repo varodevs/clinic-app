91eaf715a4ea9d89dd07b018fb5dde7d95338c40
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-13 23:40:33 +0200

    new user

--- resources/views/moreservices.blade.php
+++ resources/views/moreservices.blade.php
@@ -7,7 +7,7 @@
 </div>
     <div class="container-fluid d-flex flex-column align-items-center pt-5">
         <div class="text-center w-50"><h2>More tratments in our installations</h2></div>
-        <div class="text-center w-75"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+        <div class="text-center w-75"><p>We give you the possibility to get some special treatments.</p></div>
     </div>
 
     <div class="c-12 d-flex flex-row flex-wrap justify-content-center pt-5 _div-treat" mb-5>
@@ -16,7 +16,7 @@
             <img src="{{ asset('img/yoga.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Yoga</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Enrole in amazing yoga classes.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
@@ -24,7 +24,7 @@
             <img src="{{ asset('img/magnetoterapia.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Magnetotherapy</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Experience the healing benefits of magnets.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
@@ -32,7 +32,7 @@
             <img src="{{ asset('img/hidro.webp') }}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Hidrotherapy</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">Hidrotherapy is a great way to keep a healthy system.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
@@ -40,7 +40,7 @@
             <img src="{{ asset('img/electro.webp') }}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Electrotherapy</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">This treatment makes you recover with electricity.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>        
