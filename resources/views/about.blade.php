d36bda930f2dd63699d42bb5ed2d03cbc07550d7
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-13 23:33:40 +0200

    new user

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="container-fluid d-flex flex-column align-items-center pt-5">
         <div class="text-center w-50"><h2>Get to know us</h2></div>
-        <div class="text-center w-75"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+        <div class="text-center w-75"><p>Welcome to VitalCore, where we are dedicated to helping you achieve optimal health and wellness through personalized physiotherapy services. Our team of highly skilled and experienced physiotherapists is committed to providing exceptional care and improving your quality of life.</p></div>
     </div>
     <div class="d-flex justify-content-center col-12 mt-5">
         <img src="{{ asset('img/imagebg.jpg') }}" alt="" class="align-self-center w-75 h-25">
