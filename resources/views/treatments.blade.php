d16a187af1b8342d519d2dbe51aea8bcdfccdeb9
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-14 07:30:05 +0200

    new user

--- resources/views/treatments.blade.php
+++ resources/views/treatments.blade.php
@@ -7,7 +7,7 @@
 </div>
     <div class="container-fluid d-flex flex-column align-items-center pt-5">
         <div class="text-center w-50"><h2>Treatments we offer</h2></div>
-        <div class="text-center w-75"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+        <div class="text-center w-75"><p>In VitalCore we have several servides available to cover all your needs and ensure you receive a specialized treatment.</p></div>
     </div>
 
     <div class="c-12 d-flex flex-row flex-wrap justify-content-center pt-5 _div-treat" mb-5>
