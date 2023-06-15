0afe1900dcb579adc46069a44432a0b0a6020ac9
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-15 16:02:59 +0200

    img

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,7 +9,7 @@
         <div class="col-12 d-flex flex-column">
             <div class="col-2 align-self-start m-3">
                 <div class="d-flex flex-column justify-content-center w-50 _profile-imgDiv">
-                    <img src="{{ asset('storage/'.$img_path) }}" alt="" class="rounded-circle" height="200" width="200">
+                    <img src="{{ asset('storage/'.$img_path) }}" alt="" class="rounded-circle">
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">Username</span>
