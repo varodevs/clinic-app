8da5dc51226171032c242659d7f58c0c3d44334b
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-15 23:38:46 +0200

    profile

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -181,6 +181,46 @@
       }
       });
   </script>
+
+<script>
+  const dateInput = document.getElementById('date');
+  const hourDropdown = document.getElementById('hourDropdown');
+  const special = document.getElementById('spec');
+
+  // Function to populate the hour dropdown based on the selected date
+  function populateHourDropdown() {
+      const selectedDate = dateInput.value;
+      const selectedSpec = special.value;
+
+      // Make an AJAX request to check for available hours
+      $.ajax({
+          url: "{{ route('check') }}",
+          type: 'GET',
+          data: {
+              date: selectedDate,
+              idemp: selectedSpec
+          },
+          success: function(response) {
+              // Clear previous options
+              hourDropdown.innerHTML = '';
+
+              // Populate the dropdown with available hours
+              response.forEach(function(hour) {
+                  const option = document.createElement('option');
+                  option.value = hour;
+                  option.textContent = hour;
+                  hourDropdown.appendChild(option);
+              });
+          },
+          error: function(xhr) {
+              console.log(xhr.responseText);
+          }
+      });
+  }
+
+  // Attach change event listener to the date input field
+  dateInput.addEventListener('change', populateHourDropdown);
+</script>
     <script src="https://kit.fontawesome.com/6e079c207d.js" crossorigin="anonymous"></script>
     @if(isset($scrollToSection))
     <script>
--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -87,43 +87,4 @@
             </div>
         </div>
     </div>
-        <script>
-            const dateInput = document.getElementById('date');
-            const hourDropdown = document.getElementById('hourDropdown');
-            const special = document.getElementById('spec');
-
-            // Function to populate the hour dropdown based on the selected date
-            function populateHourDropdown() {
-                const selectedDate = dateInput.value;
-                const selectedSpec = special.value;
-
-                // Make an AJAX request to check for available hours
-                $.ajax({
-                    url: "{{ route('check') }}",
-                    type: 'GET',
-                    data: {
-                        date: selectedDate,
-                        idemp: selectedSpec
-                    },
-                    success: function(response) {
-                        // Clear previous options
-                        hourDropdown.innerHTML = '';
-
-                        // Populate the dropdown with available hours
-                        response.forEach(function(hour) {
-                            const option = document.createElement('option');
-                            option.value = hour;
-                            option.textContent = hour;
-                            hourDropdown.appendChild(option);
-                        });
-                    },
-                    error: function(xhr) {
-                        console.log(xhr.responseText);
-                    }
-                });
-            }
-
-            // Attach change event listener to the date input field
-            dateInput.addEventListener('change', populateHourDropdown);
-        </script>
 @endsection
