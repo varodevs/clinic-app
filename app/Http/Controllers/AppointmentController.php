58fd8e88f15bc2332d746c02cb11e99f1da132cc
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-16 00:59:01 +0200

    hours

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -11,22 +11,44 @@ namespace App\Http\Controllers;
         {
             $selectedDate = $request->input('date');
             $selectedSpec = $request->input('idemp');
-    
-            // Get appointments by employee ID and selected date
-            $appointments = Appointment::where('employee_id', $selectedSpec)
-                ->whereDate('date_appoint', $selectedDate)
-                ->get();
-    
-            $hours = ['08:30:00', '09:00:00', '09:30:00', '10:00:00', '10:30:00', '11:00:00', '11:30:00', '12:00:00', '12:30:00', '13:00:00', '17:00:00', '17:30:00', '18:00:00', '18:30:00', '19:00:00', '19:30:00', '20:00:00'];
-            $takenHours = [];
-    
-            foreach ($appointments as $appointment) {
-                $appointmentTime = Carbon::parse($appointment->date_appoint)->format('H:i:s');
-                $takenHours[] = $appointmentTime;
+
+            $appoint = new Appointment();
+
+            $appoints = $appoint->getAppointsByEmp($selectedSpec);
+
+            $dates_bd = [];
+
+            $found = 0;
+
+            foreach($appoints as $appo)
+            {
+                $date = $appo->date_appoint;
+                $carbonDate = Carbon::createFromFormat('Y-m-d', $selectedDate);
+                $carbonDate2 = Carbon::createFromFormat('Y-m-d', $date);
+                $formattedSelectedDate = $carbonDate->format('Y-m-d');
+                $formattedDate2 = $carbonDate2->format('Y-m-d');
+                $formattedDateTime = $date->format('Y-m-d H:i:s');
+                
+                if($formattedSelectedDate == $formattedDate2){
+                    $found++;
+                    array_push($dates_bd, $formattedDateTime);
+                }
             }
+            
+            $hours = ['08:30:00','09:00:00','09:30:00','10:00:00','10:30:00','11:00:00','11:30:00','12:00:00','12:30:00','13:00:00','17:00:00','17:30:00','18:00:00','18:30:00','19:00:00','19:30:00','20:00:00'];
+            
+            if($found != 0){
+                $taken = [];
+                foreach($dates_bd as $date){
+                    $carbonDate = Carbon::createFromFormat('Y-m-d H:i:s', $date);
+                    $hour = $carbonDate->format('H:i:s');
     
-            $availableHours = array_diff($hours, $takenHours);
-    
+                    array_push($taken, $hour);
+                }  
+                $availableHours = array_diff($hours, $taken);
+            }else{
+                $availableHours = $hours;
+            }
             return response()->json($availableHours);
         }
     }
