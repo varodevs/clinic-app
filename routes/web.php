f32691b8a6891654865b378001b70c63fad4b824
Alvaro Barba <a20barfera7699@ies-mardecadiz.com>
2023-06-16 09:19:05 +0200

    address

--- routes/web.php
+++ routes/web.php
@@ -210,7 +210,7 @@ Route::post('user/modified', [UserController::class, 'modUser'])->name('upd-pat'
 
 Route::post('user/img-updated', [UserController::class, 'uplImg'])->name('mod-img');
 
-Route::post('user/new-address', [UserController::class, 'addAddr'])->name('add-addr');
+Route::post('user/new-address', [UserController::class, 'addAddr'])->name('add-address');
 
 
 
