a2684c7c45fbcfc782766689117d9e965035ba71
Alvaro Barba <alvarobarbafer@gmail.com>
2024-09-07 23:34:06 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-                host: 'ec2-18-213-223-134.compute-1.amazonaws.com',
-            },
     plugins: [
         laravel({
             input: ['resources/scss/app.scss',
@@ -14,4 +11,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+          input: 'public\index.php',
+        },
+      },
 });
