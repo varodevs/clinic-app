6084ff365420b85f54e8e8060e62904d1df04af4
Alvaro Barba <alvarobarbafer@gmail.com>
2024-09-08 14:00:44 +0200

    more config for vite images

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="{{ asset("/img/favicon-16x16.png") }}">
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/images/photo.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>VitalCore Physio</title>
 </head>
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         }),
     ],
     build: {
+        assetsInlineLimit: 0,
         outDir: 'public/build',
         manifest: true,
         rollupOptions: {
